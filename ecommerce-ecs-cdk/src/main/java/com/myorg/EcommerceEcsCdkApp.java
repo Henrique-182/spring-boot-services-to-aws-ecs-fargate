40914e28bf8930ef766788ccd1a4c8c9a42cb8b0
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-04-30 13:54:06 -0300

    Creating the First CloudFormation stack in CDK project

---
+++ ecommerce-ecs-cdk/src/main/java/com/myorg/EcommerceEcsCdkApp.java
@@ -0,0 +1,13 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+
+public class EcommerceEcsCdkApp {
+	
+    public static void main(final String[] args) {
+        App app = new App();
+
+        app.synth();
+    }
+}
+
