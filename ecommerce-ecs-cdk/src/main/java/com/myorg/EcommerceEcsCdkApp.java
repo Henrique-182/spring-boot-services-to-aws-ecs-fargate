fbbad5b2db25c9b45db9a455ccf2872154e360d7
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-04-30 14:37:40 -0300

    Organizing the stack in the CDK project

--- ecommerce-ecs-cdk/src/main/java/com/myorg/EcommerceEcsCdkApp.java
+++ ecommerce-ecs-cdk/src/main/java/com/myorg/EcommerceEcsCdkApp.java
@@ -1,11 +1,34 @@
 package com.myorg;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
 
 public class EcommerceEcsCdkApp {
 	
     public static void main(final String[] args) {
         App app = new App();
+        
+        Environment environment = Environment.builder()
+        		.account("533267160963")
+        		.region("us-east-1")
+        		.build();
+        
+        Map<String, String> infraTags = new HashMap<>();
+        infraTags.put("team", "SiecolaCode");
+        infraTags.put("cost", "ECommerceInfra");
+        		
+        EcrStack ecrStack = new EcrStack(
+        		app, 
+        		"Ecr", 
+        		StackProps.builder()
+        			.env(environment)
+        			.tags(infraTags)
+        			.build()
+        	);
 
         app.synth();
     }
