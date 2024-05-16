559e30d7ffae67756099a57bffd281f4b30452f3
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-05-16 08:05:36 -0300

    Creating the service log driver

--- ecommerce-ecs-cdk/src/main/java/com/myorg/ProductsServiceStack.java
+++ ecommerce-ecs-cdk/src/main/java/com/myorg/ProductsServiceStack.java
@@ -1,14 +1,20 @@
 package com.myorg;
 
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.AwsLogDriver;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.FargateTaskDefinition;
 import software.amazon.awscdk.services.ecs.FargateTaskDefinitionProps;
 import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationLoadBalancer;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.LogGroupProps;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.constructs.Construct;
 
 public class ProductsServiceStack extends Stack {
@@ -24,6 +30,23 @@ public class ProductsServiceStack extends Stack {
 					.family("products-service")
 					.cpu(512)
 					.memoryLimitMiB(1024)
+					.build()
+			);
+		
+		AwsLogDriver awsLogDriver = new AwsLogDriver(
+				AwsLogDriverProps.builder()
+					.logGroup(
+						new LogGroup(
+							this, 
+							"LogGroup",
+							LogGroupProps.builder()
+								.logGroupName("ProductsService")
+								.removalPolicy(RemovalPolicy.DESTROY)
+								.retention(RetentionDays.ONE_MONTH)
+								.build()
+						)
+					)
+					.streamPrefix("ProductsService")
 					.build()
 			);
 	}
